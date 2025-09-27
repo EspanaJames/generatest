8f819aabba923296053b1b317051b3324a927665
unknown <jameshongayoespana@gmail.com>
2025-09-27 18:54:58 +0800

    initial push

---
+++ html/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name = "viewport" content="width=device-width, initial-scale=1.0">
+    <meta name = "description" content = "">
+    <meta name = "keywords" content = "">
+    <meta name = "author" content = "James España">
+    <meta http-equiv = "refresh" content = "30">
+    <link rel = "stylesheet" href ="../css/indexStyles.css">
+    <title>Generatest</title>
+</head>
+<body>
+    <section class = "header">
+        <nav class = "navBar">
+            <div class = "titleBar">
+                <a><img src="../icons/jamesLogo.png" alt="icon for logo"></a>
+                <p>GENERATEST</p>
+            </div>
+            <div class = "navButtons">
+                <ul>
+                    <li>
+                        <button><img src="../icons/jamesLogo.png" alt="dashboard logo"></button>
+                    </li>
+                    <li>
+                        <button><img src="../icons/jamesLogo.png" alt="output logo"></button>
+                    </li>
+                    <li>
+                        <button><img src="../icons/jamesLogo.png" alt="website information logo"></button>
+                    </li>
+                    <li>
+                        <button><img src="../icons/jamesLogo.png" alt="account logo"></button>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </section>
+    <section class = "content"></section>
+    <section class = "footer"></section>
+</body>
+</html>
